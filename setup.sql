fc814b81ae44702e1e76f215331da4cc71ea84c9
Seandon Mooy <admin@erulabs.com>
2014-12-06 19:47:51 -0600

    setup.sql

---
+++ setup.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE todosDB;
+
+CREATE USER 'todosuser'@'localhost' IDENTIFIED BY 'todospasswd';
+GRANT ALL PRIVILEGES ON todosDB.* TO 'todosuser'@'localhost';
+
+CREATE TABLE `todosDB`.`todos` (
+	`id` int(11) NOT NULL auto_increment,
+	`task` TEXT,
+	`dateAdded` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`dateCompleted` DEFAULT NULL,
+	`priority` SET('low', 'medium', 'high')
+primary KEY (id));
